c8f9c083afc986bdba9cbc21835a896563a04193
JefferNg <jefferng11@gmail.com>
2023-08-12 18:37:29 -0600

    created working paddles

---
+++ Project Pong/Assets/Scripts/PaddleMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour
+{
+    public float speed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tag.Equals("Player1"))
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y + speed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y + -speed * Time.deltaTime);
+            }
+        }
+        if (tag.Equals("Player2"))
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y + speed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y + -speed * Time.deltaTime);
+            }
+        }
+    }
+}
