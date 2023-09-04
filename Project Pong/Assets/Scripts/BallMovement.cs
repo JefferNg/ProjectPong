f30527eebf18f38989e9c17d3413bcf2b5a628d2
Brian Dang <pandabrian1@gmail.com>
2023-09-04 17:49:53 -0600

    Fixed Bugs

--- Project Pong/Assets/Scripts/BallMovement.cs
+++ Project Pong/Assets/Scripts/BallMovement.cs
@@ -6,7 +6,7 @@ using static UnityEngine.Rendering.VirtualTexturing.Debugging;
 public class BallMovement : MonoBehaviour
 {
     //[SerializeField] float speed = 10f;
-    [SerializeField] float maxVelocity = 40f;
+    [SerializeField] float maxVelocity = 0f;
     private ballState state = ballState.start;
     Rigidbody rb;
 
@@ -26,26 +26,63 @@ public class BallMovement : MonoBehaviour
                 if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
                 {
                     state = ballState.playing;
-                    float max = 15f;
-                    rb.velocity = new Vector2(Random.Range(-10, 10) + Mathf.Clamp(rb.velocity.x, -max, max), 10f);
+                    int ranNum = Random.Range(0, 2);
+
+                    if (ranNum == 0)
+                    {
+                        rb.AddForce(Vector3.up * maxVelocity);
+                    }
+                    else
+                    {
+                        rb.AddForce(Vector3.down * maxVelocity);
+                    }
+
+                    ranNum = Random.Range(0, 2);
+
+                    if (ranNum == 0)
+                    {
+                        rb.AddForce(Vector3.left * maxVelocity);
+                    }
+                    else
+                    {
+                        rb.AddForce(Vector3.right * maxVelocity);
+                    }
+
+                    //float max = 15f;
+                    //rb.velocity = new Vector2(Random.Range(-10, 10) + Mathf.Clamp(rb.velocity.x, -max, max), 10f);
                 }
                 break;
 
             case ballState.playing:
-                if (transform.position.x < -8f || transform.position.x > 8f)
+                if (transform.position.x < -10f || transform.position.x > 10f)
                 {
                     state = ballState.start;
                     transform.position = new Vector3(0, 0, 0);
+                    rb.velocity = Vector3.zero;
                 }
-                rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxVelocity, maxVelocity), Mathf.Clamp(rb.velocity.y, -maxVelocity, maxVelocity));
+                //rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxVelocity, maxVelocity), Mathf.Clamp(rb.velocity.y, -maxVelocity, maxVelocity));
                 break;
         }
+
+        Debug.Log(rb.velocity);
     }
 
     enum ballState
     {
         start,
         playing
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player1")
+        {
+            Debug.Log("Add right force");
+        }
+        else if (other.gameObject.tag == "Player2")
+        {
+            Debug.Log("Add left force");
+        }
     }
 
 }
